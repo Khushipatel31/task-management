d18e75abe0cf5f7bde5fb777392b7239c23effbd
Khushipatel31 <khushipatel200431@gmail.com>
2024-06-15 17:31:56 +0530

    error handling

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { errorHandler } = require("./middleware/errorHandling.js");
 dotenv.config();
 
 const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+app.use(errorHandler);
 
 require("./utils/dbConfig.js").getDBConnection();
 
