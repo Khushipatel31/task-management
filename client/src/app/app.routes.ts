90c196006860b137087415c587f18aa32bb6f350
Khushipatel31 <khushipatel200431@gmail.com>
2024-07-05 10:42:43 +0530

    route login register dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent as adminDashboard } from './modules/admin/components/dashboard/dashboard.component';
+import { DashboardComponent as userDashboard } from './modules/users/components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path:'register',
+        component:RegisterComponent
+      },
+      {
+        path: 'admin',
+        component: adminDashboard,
+        children: [
+          {
+            path: '',
+            component:adminDashboard ,
+          },
+          {
+            path: '',
+            redirectTo: '/admin/dashboard',
+            pathMatch: 'full',
+          },
+        ],
+      },
+      {
+        path: 'user',
+        component: userDashboard,
+        children: [
+          {
+            path: '',
+            component:userDashboard ,
+          },
+          {
+            path: '',
+            redirectTo: '/user/dashboard',
+            pathMatch: 'full',
+          },
+        ],
+      },
+   
+];
